f6bdab0f94cf0e0bfda90e3425b16b6b1e74b383
desyang <desyang@qq.com>
2025-06-08 17:14:51 +0800

    添加是否裁剪参数

--- app.py
+++ app.py
@@ -6,10 +6,15 @@ import modnet
 root_path = os.environ.get("ROOT_PATH")
 
 
-def modnet_photos_transfer(image):
+def modnet_photos_transfer(image, crop=False):
 
-    return modnet.predict(image)
+    return modnet.predict(image, crop)
 
-
-demo = gr.Interface(fn=modnet_photos_transfer, inputs="image", outputs="image")
+demo = gr.Interface(
+    fn=modnet_photos_transfer, 
+    inputs=[
+        "image",
+        gr.Checkbox(label="是否进行裁剪", value=False)
+    ],
+    outputs="image")
 demo.launch(server_name="0.0.0.0", root_path=root_path)
